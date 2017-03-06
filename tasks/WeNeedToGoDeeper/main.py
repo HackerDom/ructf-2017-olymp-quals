ebb7a87d006c90713921801b1f574de2f55e8ea5
rx00 <it.tec@me.com>
2017-03-07 02:32:25 +0500

    + fix solution, fix flag format

--- tasks/WeNeedToGoDeeper/main.py
+++ tasks/WeNeedToGoDeeper/main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from hashlib import sha1  # so insecure
-from agents_base import add_user_agent, check_user_agents
+import sqlite3
+from contextlib import contextmanager
 
 
 app = Flask(__name__)
@@ -13,9 +14,38 @@ do you know that md5 of "ructf2017" is f94f38ab82c48353773b316a7a663e21?
 FLAG = "RuCTF: HarderBetterFasterStrongerUserAgent2017"
 
 
+@contextmanager
+def db_connection():
+    conn = sqlite3.connect('user_agents.db')
+    try:
+        cursor = conn.cursor()
+        cursor\
+            .execute(
+             'CREATE TABLE IF NOT EXISTS agents (user_agent text unique)')
+        yield cursor
+    except sqlite3.IntegrityError:
+        pass
+    finally:
+        conn.commit()
+        conn.close()
+
+
+def add_user_agent(user_agent):
+    with db_connection() as cursor:
+        cursor.execute(
+            'INSERT INTO agents VALUES (?)', (user_agent,))
+
+
+def check_user_agents(user_agent):
+    with db_connection() as cursor:
+        cursor.execute(
+            'SELECT * FROM agents WHERE user_agent=?', (user_agent,))
+        return cursor.fetchone() is not None
+
+
 @app.route("/")
 def root_router():
-    return "Srly?! I hate guests, get out!", "200 goto root plus {}"\
+    return "Srly?! I hate guests, get out!", "200 go to root plus {}"\
         .format(sha1(STARTING_STRING.encode()).hexdigest())
 
 
@@ -28,36 +58,25 @@ def task_router(some_hash):
         user_agent = "0orTooBig"
     # fix all browsers, lol
     if "Mozilla" in user_agent or "Opera" in user_agent:
-        return "Your browser is banned!", "403 banned"
+        return "Your browser is banned!", "403 Browser is banned"
 
     # fix snakes
     if "python" in user_agent.lower():
-        return "S-s-s-sssnakeee is here", "403 Python also banned!"
+        return "S-s-s-sssnakeee is here", "403 Python is banned also!"
 
     # fix old user agents
     if check_user_agents(user_agent):
-        return "Your have been banned!", "403 Banned"
+        return "Your have been banned!", "403 Browser is Banned"
     else:
         add_user_agent(user_agent)
 
     # ok, flag
     if some_hash == "acab4de47c6e35f6dab7f9ee17fae17858c14f3d":
         return "Find the flag, (or me, lol)",\
-               "200 {}".format(FLAG)
+               '200 {}'.format(FLAG)
     return "Yep! You're on the right way!", "200 goto root plus {}"\
         .format(sha1((some_hash + SALT).encode()).hexdigest())
 
-"""
-def gen_last_hash():
-    answer, code = root_router()
-    hashes = code.split()[-1]
-    for _ in range(99):
-        answer, hashes = task_router(hashes)
-        hashes = hashes.split()[-1]
-    print(hashes)
-"""
-
 
 if __name__ == '__main__':
-    #gen_result()
     app.run("0.0.0.0", port=8080)
--- tasks/WeNeedToGoDeeper/solution.py
+++ tasks/WeNeedToGoDeeper/solution.py
@@ -2,20 +2,18 @@ from urllib.request import urlopen, Request
 import sys
 
 
-def get_next_hash(hash_str, const):
+def get_next_message(hash_str, const):
     new_request = Request(
         url="http://{}/".format(sys.argv[1]) + hash_str,
-        headers={
-            'User-Agent': "snake3.smth + {}".format(const)
-        }
+        headers={'User-Agent': "snake3.smth + {}".format(const)}
     )
-    return urlopen(new_request).msg.split()[-1]
+    return urlopen(new_request).msg
 
 
-global_hash_sting = ""
 i = 0
-while "RuCTF" not in global_hash_sting:
+global_hash_string = get_next_message("", i)
+while "RuCTF" not in global_hash_string:
     i += 1
-    print(i, global_hash_sting)
-    global_hash_sting = get_next_hash(global_hash_sting, i)
-print(global_hash_sting)
+    print(i, global_hash_string)
+    global_hash_string = get_next_message(global_hash_string.split()[-1], i)
+print(global_hash_string)
